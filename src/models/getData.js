fe3b7ec3a0983196a535a2eb6b6506e26509193a
Adam Jagosz <adamjagosz@gmail.com>
2019-10-22 17:56:58 +0200

    ignore eslint

--- src/models/getData.js
+++ src/models/getData.js
@@ -9,6 +9,7 @@ export default async function getData(language) {
   const response = await fetch(url);
   const data = await response.text();
   if (data.indexOf('<!DOCTYPE html>') > -1) {
+    // eslint-disable-next-line no-console
     console.error(`Request failed: ${language}`);
     return '';
   }
