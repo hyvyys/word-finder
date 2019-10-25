93a4058a33cfa32678a1c1aa130d8e2d84ea51b1
Adam Jagosz <adamjagosz@gmail.com>
2019-10-25 15:21:49 +0200

    update accent keyboard: add Cyrillic and Greek

--- src/components/AccentKeyboard.vue
+++ src/components/AccentKeyboard.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="accent-keyboard" v-show="!isCollapsed">
-    <UiButton class="btn btn-desktop" @click="toggleCapsLock">⇪</UiButton>
+    <div class="btns-desktop">
+      <UiButton class="btn" @click="nextCharset">{{ charsetSymbol }}</UiButton>
+      <UiButton class="btn btn-icon" @click="toggleCapsLock">⇪</UiButton>
+    </div>
   
     <div class="keys">
       <UiButton v-for="l in accents" :key="l" @click="typeAccent(l)">
@@ -9,11 +12,12 @@
     </div>
   
     <div class="btns-mq">
-      <UiButton class="btn" @click="toggle">×</UiButton>
-      <UiButton class="btn" @click="toggleCapsLock">⇪</UiButton>
+      <UiButton class="btn btn-icon" @click="toggle">×</UiButton>
+      <UiButton class="btn btn-icon" @click="toggleCapsLock">⇪</UiButton>
+      <UiButton class="btn" @click="nextCharset">{{ charsetSymbol }}</UiButton>
     </div>
   
-    <UiButton class="btn btn-desktop" @click="toggle">×</UiButton>
+    <UiButton class="btn btn-icon btns-desktop" @click="toggle">×</UiButton>
   </div>
 </template>
 
@@ -31,18 +35,46 @@ export default {
     return {
       focusedInput: null,
       capsLock: false,
-
-      accents: 
-      (
-        "áàăâåäãąāæćĉčċçďđðéèĕêěëėęēğĝġģĥħíìĭîïĩįīĳıĵķĺľļłŀńňñņŋóòŏôöőõøōœĸŕřŗßśŝšşťţŧúùŭûůüűũųūŵýŷÿźžżþ" +
-        "ſɓɗɛƒɣɨƙƚơɔƥƭưʉƴʒǀǁǂǃ"
-      ).split('').sort((a, b) => a.localeCompare(b)),
+      selectedCharset: 0,
+      charsets: [
+        {
+          name: 'Latin',
+          symbol: 'ļåťň',
+          chars: [
+            'áàăâåäãąāæćĉčċçďđðéèĕêěëėęēğĝġģĥħíìĭîïĩįīĳıĵķĺľļłŀńňñņŋóòŏôöőõøōœĸŕřŗßśŝšşťţŧúùŭûůüűũųūŵýŷÿźžżþſɓɗɛƒɣɨƙƚơɔƥƭưʉƴʒǀǁǂǃ',
+          ],
+        },
+        {
+          name: 'Cyrillic',
+          symbol: 'сўяг',
+          chars: [
+            'абвгдежзийклмнопрстуфхцчшщъыьэюя','ѐёђѓєѕіїјљњћќѝўџѡѣѥѩѭѱѳѵѷѹѻѽѿҁҋҍҏґғҕҗҙқҝҟҡңҥҧҩҫҭүұҳҵҷҹһҽҿӂӄӆӈӊӌӎӏӑӓӕӗәӛӝӟӡӣӥӧөӫӭӯӱӳӵӷӹӻӽӿ',
+          ],
+        },
+        {
+          name: 'Greek',
+          symbol: 'grek',
+          chars: [
+            'αβγδεζηθικλμνξοπρσςτυφχψω',
+          ],
+        },
+      ],
     };
   },
   computed: {
     isTextArea() {
       return this.focusedInput && this.focusedInput.tagName.toLowerCase() === 'textarea';
-    }
+    },
+    accents() {
+      return this.charsets[this.selectedCharset].chars.flatMap(a => a.split('').sort((a, b) => a.localeCompare(b)));
+    },
+    charsetSymbol() {
+      return this.charsets[this.selectedCharset].symbol;
+    },
+    nextCharsetSymbol() {
+      const i = this.getNextCharsetIndex();
+      return this.charsets[i].symbol;
+    },
   },
   created() {
     document.addEventListener('focusin', this.focusChanged);
@@ -69,6 +101,12 @@ export default {
     },
     toggleCapsLock() {
       this.capsLock = !this.capsLock;
+    },
+    nextCharset() {
+      this.selectedCharset = this.getNextCharsetIndex();
+    },
+    getNextCharsetIndex() {
+      return (this.selectedCharset + 1) % this.charsets.length;
     },
     formatAccent(l) {
       return this.capsLock ? l.toUpperCase() : l;
@@ -101,6 +139,7 @@ export default {
   left: 50%;
   width: 1200px;
   max-width: 100%;
+  height: 10rem;
   transform: translateX(-50%);
   background: $light;
   box-shadow: 0 1px 7px rgba(#333, 0.6);
@@ -115,8 +154,11 @@ export default {
   }
 
 
-  .btn {
+  .btn-icon  {
     font-size: 1.5rem;
+  }
+  .btn {
+    display: block;
     ::v-deep .ui-button__content {
       padding-bottom: 0.2rem;
     }
@@ -127,26 +169,32 @@ export default {
     grid-template-columns: auto auto;
     .btns-mq {
       height: 100%;
+      margin-right: 4px;
       display: flex;
       flex-direction: column;
       .btn {
         padding: 0 2px;
-        min-width: 2rem;
+        min-width: 2.1rem;
+        min-height: 20px;
         flex: 1;
+        &:not(:last-child) {
+          margin-bottom: 4px;
+        }
       }
     }
-    .btn-desktop {
+    .btns-desktop {
       display: none;
     }
   }
 
   .keys {
-    max-height: 25vh;
+    flex: 1;
+    max-height: 100%;
     overflow: auto;
     
     margin: 0 1rem;
     @media screen and (max-width: #{$mq-max-width}) {
-      margin: 0 4px;
+      margin: 0 4px 4px 0;
     }
 
     display: flex;
