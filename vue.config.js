dcecc62485fa129c301f5b997fbaabc99ec7272c
Adam Jagosz <adamjagosz@gmail.com>
2019-11-27 22:14:24 +0100

    add custom domain support

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-  publicPath: process.env.NODE_ENV === "production" ? "/word-finder/" : "/",
+  publicPath: process.env.NODE_ENV === "production" ? "/" : "/",
 
   css: {
     loaderOptions: {
